d7665973877f19c30be1bf03194d6901eb6e70c9
Lacci <lannadosanjoscosta@gmail.com>
2026-06-06 09:29:59 -0300

    add guard no auth + arquivo p caso a gnt use img

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,10 @@
+// add a guard no topo ao invés de verficar dentro do callback
+// assim o erro já aparece qnd sobre o server e n na primeira request autenticada
 const jwt = require('jsonwebtoken');
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET não definida!! Verifique o arquivo .env');
+}
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -8,7 +14,7 @@ const authenticateToken = (req, res, next) => {
     return res.status(401).json({ error: 'Token de autenticação não fornecido.' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET || 'supersecretjwtkey123!@#', (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ error: 'Token inválido ou expirado.' });
     }
